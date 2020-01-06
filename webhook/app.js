55f3c08b989df8012790e4855ec3843c5e39b19b
rongfengliang <1141591465@qq.com>
2020-01-06 09:32:20 +0800

    add more code for cache && api docs

--- webhook/app.js
+++ webhook/app.js
@@ -1,16 +1,74 @@
 require("dotenv").config()
+const fastify = require('fastify')({ logger: true })
+const cacheManager = require('cache-manager')
+// for cache some access token
+const memoryCache = cacheManager.caching({
+    store: "memory"
+})
+fastify.register(require('fastify-swagger'), {
+  routePrefix: '/docs',
+  swagger: {
+    info: {
+      title: 'webhok alert',
+      description: 'webhok alert',
+      version: '0.1.0'
+    },
+    host: 'localhost',
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    tags: [
+      { name: 'webhook', description: 'webhok alert' }
+    ]
+  },
+  exposeRoute: true
+})
+// Declare a route
+fastify.post('/', {
+  schema: {
+    description: 'webhook for alert',
+    tags: ['webhook'],
+    body: {
+      type: 'object',
+      properties: {
+        type: { type: 'string' },
+        status: { type: 'string' },
+        time: { type: 'string' },
+        replicas: {
+          type: "array", items: {
+            type: "string"
+          }
+        },
+        page: {
+          type: 'object',
+          properties: {
+            title: { type: 'string' },
+            url: { type: 'string' },
+          }
+        }
+      }
+    },
+    response: {
+      201: {
+        description: 'Successful response',
+        type: 'object',
+        properties: {
+          status: { type: 'string' }
+        }
+      }
+    }
+  }
+}, (request, reply) => {
+  request.log.info(JSON.stringify(request.body))
+  reply.send({ hello: JSON.stringify(request.body) })
+})
 
-const fastify = require('fastify')({
-    logger: true
-  })
-  // Declare a route
-  fastify.post('/', (request, reply) => {
-    request.log.info(JSON.stringify(request.body))
-    reply.send({ hello: JSON.stringify(request.body) })
-  })
-  
-  // Run the server!
-  fastify.listen(3000,"0.0.0.0",(err, address) => {
-    if (err) throw err
-    fastify.log.info(`server listening on ${address}`)
-  })
+fastify.ready(err => {
+  if (err) throw err
+  fastify.swagger()
+})
+// Run the server!
+fastify.listen(3000, "0.0.0.0", (err, address) => {
+  if (err) throw err
+  fastify.log.info(`server listening on ${address}`)
+})
